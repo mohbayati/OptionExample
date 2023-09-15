670d94c3b2b1f9576015aafbf6585d9ceac05cdd
Mohammad Bayati <Mohammad.Bayati@SLATERGORDON.UK>
2023-09-15 16:28:50 +0100

    Added validation into options

--- OptionExample/ApplicationOptions.cs
+++ OptionExample/ApplicationOptions.cs
@@ -1,6 +1,9 @@
-﻿namespace OptionExample;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OptionExample;
 
 public class ApplicationOptions
 {
+    [Required]
     public string ExampleValue { get; set; } = string.Empty;
 }
--- OptionExample/Program.cs
+++ OptionExample/Program.cs
@@ -5,25 +5,39 @@ using OptionExample;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+//this one can use without ApplicationOptionSetup
 //builder.Services.Configure<ApplicationOptions>(
 //    builder.Configuration.GetSection(nameof(ApplicationOptions)));
 
-builder.Services.ConfigureOptions<ApplicationOptionSetup>(); 
+//builder.Services.ConfigureOptions<ApplicationOptionSetup>();
+
+builder.Services.AddOptions<ApplicationOptions>()
+            .BindConfiguration(nameof(ApplicationOptions))
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
+builder.Services.AddScoped(sp => sp.GetRequiredService<IOptionsSnapshot<ApplicationOptions>>().Value);
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.MapGet("options", (IOptions<ApplicationOptions> options,
-    IOptionsSnapshot<ApplicationOptions> optionsSnapshot,
-    IOptionsMonitor<ApplicationOptions> optionsMonitor) => {
-        var response = new
-        {
-            OptionValue = options.Value.ExampleValue,
-            SnapshotValue = optionsSnapshot.Value.ExampleValue,
-            MonitorValue = optionsMonitor.CurrentValue.ExampleValue
-    };
-    return Results.Ok(response); 
+//app.MapGet("options", (IOptions<ApplicationOptions> options,
+//    IOptionsSnapshot<ApplicationOptions> optionsSnapshot,
+//    IOptionsMonitor<ApplicationOptions> optionsMonitor) => {
+//        var response = new
+//        {
+//            OptionValue = options.Value.ExampleValue,
+//            SnapshotValue = optionsSnapshot.Value.ExampleValue,
+//            MonitorValue = optionsMonitor.CurrentValue.ExampleValue
+//    };
+//    return Results.Ok(response); 
+//});
+
+app.MapGet("/", (ApplicationOptions options) =>
+{
+    return Results.Ok(options);
 });
 
 app.Run();
